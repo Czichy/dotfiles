347dce4b5377bf147e0477967ac2aac1573c19ba
Christian Czichy <privat@czichy.com>
2018-08-03 12:34:05 +0200

    nvim

--- .zsh/autorun-same-tmux.zsh
+++ .zsh/autorun-same-tmux.zsh
@@ -1,12 +1,15 @@
-#if [[ -z "$TMUX" ]]; then
-#  tmux has-session &> /dev/null
-#  if [ $? -eq 1 ]; then
-#    exec tmux new
-#  else
-#    exec tmux attach
-#  fi
-#  #fi
-if [ -z "$TMUX" ]
-then
-    tmux attach -t TMUX || tmux new -s TMUX
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+else
+  if [[ -z "$TMUX" ]]; then
+    tmux has-session &> /dev/null
+    if [ $? -eq 1 ]; then
+      exec tmux new -s TMUX
+    else
+      exec tmux attach -t TMUX
+    fi
+  fi
 fi
+#if [ -z "$TMUX" ]
+#then
+#    tmux attach -t TMUX || tmux new -s TMUX
+#fi
